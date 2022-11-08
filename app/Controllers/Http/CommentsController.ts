60d33b4d0736b388449ded6f9ecd6fb8bcd9549e
xanda <xanda@CARLOSALEXANDRE>
2022-11-08 11:02:14 -0300

    first commit

---
+++ app/Controllers/Http/CommentsController.ts
@@ -0,0 +1,18 @@
+ import { HttpContext } from '@adonisjs/core/build/standalone'
+ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+
+
+ public async store({require,params, response}: HttpContextContract){
+
+    const body = request.body()
+    const momentId = params.momentId
+
+
+
+
+
+
+ }
+
+export default class CommentsController {}
